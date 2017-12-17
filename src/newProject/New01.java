656f5f5507a9413399e8b1d68f57f77e9ec8b7c3
[Nabila] <[nazrulislamdobon@gmail.com]>
2017-12-16 20:07:44 -0500

    I have done some changes in WebProjeect1001

---
+++ src/newProject/New01.java
@@ -0,0 +1,12 @@
+package newProject;
+
+public class New01 {
+
+	public static void main(String[] args) {
+		System.out.println(" HI my name is Md Nazrul Islam ");
+		System.out.print(" This is  my first time java programming");
+		System.out.print("\nI,m really like this program");
+		System.out.print("\nI will always try to do my best");
+	}
+
+}
